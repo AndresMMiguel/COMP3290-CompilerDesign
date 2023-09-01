410772bcfdab9216d7cf05e3fa5e678329f636db
CSwift-2023 <122491622+CSwift-2023@users.noreply.github.com>
2023-09-01 14:58:09 +1000

    Add files via upload

---
+++ SyntaxNode.java
@@ -0,0 +1,48 @@
+public class SyntaxNode {
+    
+    private String symbolValue;
+    private String symbolType;
+    private SyntaxNode left;
+    private SyntaxNode middle;
+    private SyntaxNode right;
+
+    public SyntaxNode(String value, String type){
+        this.symbolValue = value;
+        this.symbolType = type;
+        left = null;
+        middle = null;
+        right = null;
+    }
+
+    public void setLeft(SyntaxNode right){
+        this.right = right;
+    }
+
+    public void setMIddle(SyntaxNode middle){
+        this.middle = middle;
+    }
+
+    public void setRight(SyntaxNode right){
+        this.right = right;
+    }
+
+    public SyntaxNode getLeft(){
+        return this.left;
+    }
+
+    public SyntaxNode getMiddle(){
+        return this.middle;
+    }
+
+    public SyntaxNode getRight(){
+        return this.right;
+    }   
+
+    public String getSymbolValue(){
+        return symbolValue;
+    }
+
+    public String getSymbolType(){
+        return symbolType;
+    }
+}
